98b381478a4b5659c3ed29eaa9d15b5a6a88ccfe
Maksim Morozov <maxmorozov.art@gmail.com>
2021-10-14 15:46:39 +0300

    Add demo access section

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Header from '../Header';
+import bgUrl from '../../assets/images/bg.png';
+import styles from './App.module.scss';
 
 const App = () => {
   return (
     <div>
       <Header />
-      <main></main>
+      <main>
+        <section className={styles['demo-access']}>
+          <img className={styles['demo-access__bg']} src={bgUrl} alt="" />
+          <h2 className={styles['demo-access__title']}>
+            Запросить демо-доступ
+          </h2>
+          <p>
+            Доступ к платформе возможен исключительно для представителей,
+            юридических лиц или индивидуальных предпринимателей
+          </p>
+        </section>
+      </main>
     </div>
   );
 };
